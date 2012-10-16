85669ccd889fb5fb5154320dd8e3c31e8bd1e764
Greg Hendershott <greghendershott@gmail.com>
2012-10-15 21:23:41 -0400

    Update example to use `paged' and #:key default.

--- examples/macro/plus.rkt
+++ examples/macro/plus.rkt
@@ -3,6 +3,6 @@
 (require gapi/macro)
 (require-gapi-doc "plus.v1.js")
 
-(define js (plus.people.search "Greg Hendershott" #:key (api-key)))
+(define js (paged (plus.people.search "Greg Henderson")))
 (for/list ([x (hash-ref js 'items (hasheq))])
   (hash-ref x 'displayName))
