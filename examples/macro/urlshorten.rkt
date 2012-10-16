70390d159f88c91c458bebdcaf93ada9f67e4ae2
Greg Hendershott <greghendershott@gmail.com>
2012-10-16 10:30:58 -0400

    Update macro example to use default #:key

--- examples/macro/urlshorten.rkt
+++ examples/macro/urlshorten.rkt
@@ -1,15 +1,12 @@
 #lang racket
 
 (require gapi/macro)
-
 (require-gapi-doc "urlshortener.v1.js")
 
 (define orig-url "http://www.racket-lang.org/")
-(define js-insert (urlshortener.url.insert #:longUrl orig-url
-                                           #:key (api-key)))
+(define js-insert (urlshortener-url-insert #:longUrl orig-url))
 (define short-url (dict-ref js-insert 'id))
-(define js-get (urlshortener.url.get short-url
-                                     #:key (api-key)))
+(define js-get (urlshortener-url-get short-url))
 (define long-url (dict-ref js-get 'longUrl))
 (printf "~s was shortened to ~s, which expanded back to ~s: ~a"
         orig-url short-url long-url
