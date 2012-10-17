691eb0a4ad742d530b7c3c6bf957ee9acbb79006
Greg Hendershott <greghendershott@gmail.com>
2012-10-17 11:45:12 -0400

    More helpful message when ~/.google-api-key doesn't exist.

--- main.rkt
+++ main.rkt
@@ -115,9 +115,12 @@
 ;; Read an API key from a dot file, defaulting to ~/.google-api-key
 (define (read-api-key [file (build-path (find-system-path 'home-dir)
                                         ".google-api-key")])
-  (match (file->string file #:mode 'text)
-    [(regexp "^\\s*(.*?)\\s*(?:[\r\n]*)$" (list _ k)) k]
-    [else (error 'read-api-key "Bad format for ~a" file)]))
+  (cond [(file-exists? file)
+         (match (file->string file #:mode 'text)
+           [(regexp "^\\s*(.*?)\\s*(?:[\r\n]*)$" (list _ k)) k]
+           [else (error 'read-api-key "Bad format for ~a" file)])]
+        [else (error 'read-api-key "Put your Google API Key in ~a" file)]))
+
 (define api-key (make-parameter (read-api-key)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
