greghendershott/gapi

commit 012cbe55b0269fd5a00dc3c5ad68bd704734b771

Fix problems with Scribbled examples. Closes #1.

manual.scrbl
@@ -64,7 +64,8 @@ name is @tt{urlshortener.v1.js}.
 instance:
 
 @codeblock{
-(require gapi/macro)
+#lang racket
+(require (planet gh/gapi/macro)
 (require-gapi-doc "urlshortener.v1.js")
 }
 
@@ -75,7 +76,7 @@ For example:
 @codeblock{
 ;; Use goo.gl to make a short URI
 (define js (urlshortener-url-insert #:longUrl "http://www.racket-lang.org/"))
-(dict-ref js-insert 'id)
+(dict-ref js 'id)
 }
 
 In this example, the @racket[urlshortener-url-insert] procedure was
@@ -183,13 +184,15 @@ available service documents are:
 Example:
 
 @codeblock{
-(require gapi/macro)
+#lang racket
+
+(require (planet gh/gapi/macro))
 (require-gapi-doc "urlshortener.v1.js")
 
 (define orig-url "http://www.racket-lang.org/")
 (define js-insert (urlshortener-url-insert #:longUrl orig-url))
 (define short-url (dict-ref js-insert 'id))
-(define js-get (urlshortener-url-get short-url))
+(define js-get (urlshortener-url-get #:shortUrl short-url))
 (define long-url (dict-ref js-get 'longUrl))
 (printf "~s was shortened to ~s, which expanded back to ~s: ~a"
         orig-url short-url long-url

commit 62e788cb7207483bf258ddb23b761ed4441c13ff

Add missing close paren
Mistake introduced in 012cbe55b0269fd5a00dc3c5ad68bd704734b771

info.rkt
@@ -10,8 +10,8 @@
 (define homepage "https://github.com/greghendershott/gapi")
 
 (define release-notes
-  '((p "Initial version. If PLaneT allowed 0.x version numbers, I'd probably call this 0.8.")))
-(define version "2012-10-16")
+  '((p "A first-24-hour update. This version still needs more examples. It also has a mysterious issue when used in the REPL, but should be fine when used in a .rkt file.")))
+(define version "2012-10-18")
 (define can-be-loaded-with 'all)
 
 (define primary-file '("main.rkt"))

manual.scrbl
@@ -65,7 +65,7 @@ instance:
 
 @codeblock{
 #lang racket
-(require (planet gh/gapi/macro)
+(require (planet gh/gapi/macro))
 (require-gapi-doc "urlshortener.v1.js")
 }
 