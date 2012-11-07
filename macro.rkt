eee42c0bf10ba47d9a0aee1b796cb405ac414e72
Greg Hendershott <greghendershott@gmail.com>
2012-11-07 16:22:45 -0500

    Totally refactor the macro approach.
    Instead of duplicating code in dynamic.rkt, completely
    reuse it in the macro implementation.

--- macro.rkt
+++ macro.rkt
@@ -4,117 +4,36 @@
                      racket/match
                      racket/list
                      json
-                     "main.rkt")
+                     "dynamic.rkt")
          net/url
          json
          net/uri-codec
-         "main.rkt"
-         "patch-port.rkt")
+         "dynamic.rkt")
 
 (provide require-gapi-doc
          api-key
-         (all-from-out "main.rkt"))
+         (all-from-out "dynamic.rkt"))
 
-(begin-for-syntax
+(define-for-syntax (proc-name stx method)
+  (define id (hash-ref method 'id))
+  (define sym (string->symbol (regexp-replace* #rx"\\." id "-")))
+  (datum->syntax stx sym))
 
- (define (gen-racket js stx)
-   #`(begin
-       #,@(append*
-           (for/list ([(k v) (hash-ref js 'resources)])
-             (for/list ([(k v) (hash-ref v 'methods)])
-               (do-method stx js v))))))
- 
-  (define (do-method stx root meth)
-    (define name (string->symbol
-                  (regexp-replace* #rx"\\." (hash-ref meth 'id) "-")))
-    (define api-params (hash-ref root 'parameters))
-    (define api-param-names (hash-keys api-params))
-    (define params (hash-ref meth 'parameters (hash)))
-    (define (required? x)
-      (and (hash-has-key? x 'required)
-           (hash-ref x 'required)))
-    (define req-params (for/hasheq ([(k v) params]
-                                    #:when (required? v))
-                         (values k v)))
-    (define opt-params (for/hasheq ([(k v) params]
-                                    #:when (not (required? v)))
-                         (values k v)))
-    (define req-param-names (hash-keys req-params))
-    (define opt-param-names (hash-keys opt-params))
-    (define _body-params
-      (hash-ref (hash-ref (hash-ref root 'schemas)
-                          (string->symbol
-                           (hash-ref (hash-ref meth 'request (hash)) '$ref ""))
-                          (hash))
-                'properties
-                (hash)))
-    ;; The "licensing" API has a problem where it duplicates a
-    ;; required parameter (such as "userID" or "productID") in the
-    ;; body parameters. Filter such problems here.
-    (define body-params
-      (for/hasheq ([(k v) _body-params]
-                   #:when (not (or (hash-has-key? req-params k)
-                                   (hash-has-key? opt-params k)
-                                   (hash-has-key? api-params k))))
-        (values k v)))
-    (define body-param-names (hash-keys body-params))
-    (define all-opt-param-names (append opt-param-names
-                                        body-param-names
-                                        api-param-names))
-    (define qps (append req-param-names opt-param-names api-param-names))
-    (define symbol->keyword (compose1 string->keyword symbol->string))
-    (define base-uri (hash-ref root 'baseUrl))
-    (define res-path (hash-ref meth 'path))
-    #`(define (#,(datum->syntax stx name)
-               [headers '()]
-               #,@(append* (map (lambda (x)
-                                  (list (symbol->keyword x)
-                                        x))
-                                req-param-names))
-               #,@(append* (map (lambda (x)
-                                  (list (symbol->keyword x)
-                                        (list x
-                                              (cond [(eq? x 'key)
-                                                     #'(api-key)]
-                                                    [else ''N/A]))))
-                                all-opt-param-names)))
-        (define _qpstr (alist->form-urlencoded
-                        (filter-map
-                         (lambda (k v)
-                           (cond [(eq? v 'N/A) #f]
-                                 [else (cons (string->symbol k) v)]))
-                         (list #,@(map symbol->string qps))
-                         (list #,@qps))))
-        (define qpstr (cond [(equal? _qpstr "") ""]
-                            [else (string-append "?" _qpstr)]))
-        (define url (string->url (string-append #,base-uri #,res-path qpstr)))
-        (define h (cons "Content-Type: application/json" headers))
-        (define body
-          (jsexpr->bytes
-           (for/hasheq ([k (list #,@(map symbol->string body-param-names))]
-                        [v (list #,@body-param-names)]
-                        #:when (not (eq? v 'N/A)))
-             (values (string->symbol k) v)))) 
-        (define in
-          #,(match (hash-ref meth 'httpMethod)
-              ["GET" #'(get-pure-port url h)]
-              ["POST" #'(post-pure-port url body h)]
-              ["PUT" #'(put-pure-port url body h)]
-              ["DELETE" #'(delete-pure-port url h)]
-              ["PATCH" #'(patch-pure-port url body h)]
-              [(var m) (lambda (m)
-                         #'(error ',name "HTTP method ~a not supported" m))]))
-        (define js (read-json in))
-        (close-input-port in)
-        js))
-
-  (define (gen gen)
-    (define (m stx)
-      (syntax-case stx ()
-        [(_ js-file)
-         (or (string? (syntax-e #'js-file))
-             (symbol? (syntax-e #'js-file)))
-         (gen (load-discovery-document (syntax-e #'js-file)) stx)]))
-    m))
-
-(define-syntax require-gapi-doc (gen gen-racket))
+(define-syntax (require-gapi-doc stx)
+  (syntax-case stx ()
+    [(_ file)
+     (or (string? (syntax-e #'file))
+         (symbol? (syntax-e #'file)))
+     (let ([dd (load-discovery-document (syntax-e #'file))])
+       (with-syntax ([dd/stx (datum->syntax stx dd)])
+         #`(begin
+             (define service (discovery-document->service dd/stx))
+             #,@(append*
+                 (for/list ([(rn rv) (hash-ref dd 'resources)])
+                   (for/list ([(mn mv) (hash-ref rv 'methods)])
+                     (with-syntax ([name (proc-name stx mv)])
+                       #`(begin
+                           (define
+                             name
+                             (method-proc service (quote #,rn) (quote #,mn)))
+                           (provide name)))))))))]))
