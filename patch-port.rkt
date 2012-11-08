67d301328c284a0d2c594b59b6f3d46e4ee37e01
Greg Hendershott <greghendershott@gmail.com>
2012-11-07 20:03:38 -0500

    Enable patch-pure-port to use SSL.
    Prior commit overlooked net/url's way of supporting SSL
    via overriding tcp-connect and tcp-abandon.

--- patch-port.rkt
+++ patch-port.rkt
@@ -21,7 +21,26 @@
 
 ;; <<COPY-PASTE
 
+(require (rename-in racket/tcp
+                    [tcp-connect plain-tcp-connect]
+                    [tcp-abandon-port plain-tcp-abandon-port])
+         openssl)
+
 (define current-connect-scheme (make-parameter "http"))
+
+(define current-https-protocol (make-parameter 'sslv2-or-v3))
+
+;; Define `tcp-connect' and `tcp-abandon-port' to fit with
+;; `current-connect-scheme'
+(define (tcp-connect host port)
+  (cond [(equal? (current-connect-scheme) "https")
+         (ssl-connect host port (current-https-protocol))]
+        [else
+         (plain-tcp-connect host port)]))
+
+(define (tcp-abandon-port port)
+  (cond [(ssl-port? port) (ssl-abandon-port port)]
+        [else (plain-tcp-abandon-port port)]))
 
 (struct our-url-exception exn:fail ())
 
